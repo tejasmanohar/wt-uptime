9b3f34c291d773d5a673952b1efaa8a45d9bd93d
Tejas Manohar <me@tejas.io>
2015-08-25 20:02:15 -0500

    redo twilio interaction, don't need promisified version here

--- index.js
+++ index.js
@@ -6,20 +6,23 @@ import twilio from 'twilio';
 
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER, MY_PHONE_NUMBER, URL } = ctx.data;
 
-const twilioClient = new twilio.RestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
-const createMessage = P.promisify(twilioClient.sms.messages.create, twilioClient);
-
 export default function(ctx, cb) {
-
   if (!TWILIO_ACCOUNT_SID || !TWILIO_AUTH_TOKEN || !TWILIO_PHONE_NUMBER || !MY_PHONE_NUMBER) {
     return cb(new Error('Key missing'));
   }
 
   request.get(URL)
     .catch(() => {
-      sendText(MY_PHONE_NUMBER, 'Site down!');
+      notifyUser(MY_PHONE_NUMBER);
     }).finally(() => {
       cb();
     });
+}
 
+function sendText(phone) {
+  return twilio.sms.messages.post({
+    to: phone,
+    from: TWILIO_PHONE_NUMBER,
+    body: 'Site down!'
+  });
 }
