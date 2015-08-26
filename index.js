194cc2a2e1a1e0ed522d2d4f0ac697751ce2e1b1
Tejas Manohar <me@tejas.io>
2015-08-25 20:19:12 -0500

    scratch off 'restructuring', create fn to check requirements

--- index.js
+++ index.js
@@ -4,25 +4,38 @@ import P from 'bluebird';
 import request from 'superagent-bluebird-promise';
 import twilio from 'twilio';
 
-const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER, MY_PHONE_NUMBER, URL } = ctx.data;
+const twilioClient = new twilio.RestClient(ctx.data.TWILIO_ACCOUNT_SID, ctx.data.TWILIO_AUTH_TOKEN)
 
 export default function(ctx, cb) {
-  if (!TWILIO_ACCOUNT_SID || !TWILIO_AUTH_TOKEN || !TWILIO_PHONE_NUMBER || !MY_PHONE_NUMBER) {
-    return cb(new Error('Key missing'));
+  const reqs = meetsReqs();
+  if (!reqs.success) {
+    return cb(new Error(reqs.message));
   }
 
-  request.get(URL)
+  request.get(ctx.data.URL)
     .catch(() => {
-      notifyUser(MY_PHONE_NUMBER);
+      notifyUser();
     }).finally(() => {
       cb();
     });
 }
 
-function sendText(phone) {
-  return twilio.sms.messages.post({
-    to: phone,
-    from: TWILIO_PHONE_NUMBER,
+function meetsReqs() {
+  const keys = ['TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN', 'TWILIO_PHONE_NUMBER', 'MY_PHONE_NUMBER', 'URL'];
+  for (let key of keys) {
+    if (!ctx.data[key]) {
+      return {
+        success: false,
+        message: `${key} secret is missing!`
+      };
+    }
+  }
+}
+
+function notifyUser() {
+  return twilioClient.sms.messages.post({
+    to: MY_PHONE_NUMBER,
+    from: ctx.data.TWILIO_PHONE_NUMBER,
     body: 'Site down!'
   });
 }
