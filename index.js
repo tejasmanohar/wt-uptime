30457bb086921f37d8c17d8153133a78fa40b8aa
Tejas Manohar <me@tejas.io>
2015-08-25 19:56:15 -0500

    destructure for simpler accessing

--- index.js
+++ index.js
@@ -3,3 +3,6 @@
 import P from 'bluebird';
 import request from 'superagent-bluebird-promise';
 import twilio from 'twilio';
+
+const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER, MY_PHONE_NUMBER, URL } = ctx.data;
+
